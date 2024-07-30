14fdb4f3c3554f0ead6bf33ff464cba728159eb4
unknown <alihasnainhfd@gmail.com>
2024-07-31 00:24:51 +0500

    video sharing

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+import { useParams } from "react-router-dom";
 
 const Room = () => {
-  return (
-    <div>Room</div>
-  )
-}
+    const { roomID } = useParams();
+    const meeting = (element) => {
+        // generate Kit Token
+        const appID = Number(import.meta.env.VITE_APP_ID);
+        const serverSecret = import.meta.env.VITE_SERVER_SECRET;
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            roomID,
+            Date.now().toString(),
+            "test"
+        );
 
-export default Room
+        // Create instance object from Kit Token.
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+        // start the call
+        zp.joinRoom({
+            container: element,
+            sharedLinks: [
+                {
+                    name: 'Meeting link',
+                    url:
+                        window.location.protocol + '//' +
+                        window.location.host + window.location.pathname +
+                        '?roomID=' +
+                        roomID,
+                },
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.OneONoneCall,
+            },
+        });
+
+    };
+    return <div ref={meeting} className="w-scree h-screen" />;
+};
+
+export default Room;
